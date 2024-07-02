d060098b1ce5e9cb9e0e873a8f2464965f2fd910
AlejandraIbaB <91706597+AlejandraIbaB@users.noreply.github.com>
2024-07-02 10:17:01 -0500

    Create iframeTAV1.cy.js

---
+++ e2e/Integration/iframeTAV1.cy.js
@@ -0,0 +1,47 @@
+escribe('Home Page', () => {
+  it('should create a new task', () => {
+    cy.login();
+    cy.get('#need-help').should('exist');
+    cy.get('.navbar-header', { timeout: 10000 }).click();
+    cy.get('i[data-original-title="Asignar Tareas"]').click();
+    cy.visit('https://staging.mydatascope.com/task_assigns/new');
+
+    ///// iframe tabla de tareas - Botón crear nueva tarea
+    cy.get('iframe[id="form_iframe"]').should('exist');
+
+    // Función personalizada para obtener el cuerpo del iframe
+    Cypress.Commands.add('getIframeBody', (iframeSelector) => {
+      return cy
+        .get(iframeSelector)
+        .its('0.contentDocument.body')
+        .should('not.be.empty')
+        .then(cy.wrap);
+    });
+
+    // Usar la función personalizada para interactuar con el iframe
+    cy.getIframeBody('iframe[id="form_iframe"]').within(() => {
+      // Interactuar con los elementos dentro del iframe
+      cy.get('div[role="combobox"]')
+        .should('be.visible')
+        .click()
+
+      cy.get('input[placeholder="Buscar usuarios..."]')
+        .should('be.visible')
+        .type('Alejandra')
+        .wait (5000)
+  
+
+      cy.get('ul[role="listbox"]') // Ajusta el selector si es necesario
+        .should('be.visible')
+        .within(() => {
+
+          cy.log('Llego hasta aca');
+        });
+        
+      
+      
+    });
+
+  });
+});
+
